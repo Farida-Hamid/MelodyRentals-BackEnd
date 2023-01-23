4a8c17c3125ecd4aee2b52b4cfce16b6a4d6f22f
Elfin Git <musongoletracy@gmail.com>
2023-01-23 18:27:58 +0200

    update routes and jwt auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::API
   include JsonWebToken
 
   def not_found
-        render json: { error: 'not_found' }
+    render json: { error: 'not_found' }
   end
-    
+
   def authorize_request
     header = request.headers['Authorization']
-    header = header.split(' ').last if header
+    header = header.split.last if header
     begin
       @decoded = jwt_decode(header)
       @current_user = User.find(@decoded[:user_id])
--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module MelodyRentals
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.autoload_paths << "#{Rails.root}/lib"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
