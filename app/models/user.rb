4b6fe342e8420a7967fcb9514050d342d66ceae9
bantealem <bantem2021@gmail.com>
2023-01-23 18:03:12 +0300

    Fix Linters Error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-    has_secure_password
-    mount_uploader :avatar, AvatarUploader
-    validates :email, presence: true, uniqueness: true
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :username, presence: true, uniqueness: true
-    validates :name, presence: true, uniqueness: true
-    validates :password,
-              length: { minimum: 6 },
-              if: -> { new_record? || !password.nil? }
-    has_many :instruments, dependent: :destroy, foreign_key: :user_id
+  has_secure_password
+  mount_uploader :avatar, AvatarUploader
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  validates :password,
+            length: { minimum: 6 },
+            if: -> { new_record? || !password.nil? }
+  has_many :instruments, dependent: :destroy, foreign_key: :user_id
 end
