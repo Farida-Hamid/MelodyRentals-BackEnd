89945ea9752165d0b8f2c6bb7ef8d8255eccb7e0
Elfin Git <musongoletracy@gmail.com>
2023-01-24 10:51:34 +0200

    update instrument controller

--- app/controllers/api/v1/instruments_controller.rb
+++ app/controllers/api/v1/instruments_controller.rb
@@ -1,64 +1,73 @@
 module Api
   module V1
     class InstrumentsController < ApplicationController
-        before_action :set_instrument, only: [:show]
-        before_action :authenticate_user!, only: [:create, :update, :destroy]
+      before_action :set_instrument, only: [:show]
+      before_action :authenticate_user!, except: %i[index show]
 
-        # GET /instruments
-        def index
-            @instruments = Instrument.all
-    
-            render json: @instruments, status: :ok
+      # GET /instruments
+      def index
+        @instruments = Instrument.all
+
+        render json: @instruments, status: :ok
+      end
+
+      # GET /instruments/1
+      def show
+        render json: @instrument, status: :ok
+      end
+
+      # POST /instruments
+      def create
+        unless current_user.role == 'admin'
+          return render json: { error: 'You do not have authorization' },
+                        status: :unauthorized
         end
+        @instrument = Instrument.new(instrument_params)
 
-        # GET /instruments/1
-        def show
-            render json: @instrument, status: :ok
+        if @instrument.save
+          render json: @instrument, status: :created, location: @instrument
+        else
+          render json: @instrument.errors, status: :unprocessable_entity
         end
+      end
 
-        # POST /instruments
-        def create
-            return render json: { error: 'You do not have authorization' }, status: :unauthorized unless current_user.role == 'admin'
-            @instrument = Instrument.new(instrument_params)
-    
-            if @instrument.save
-                render json: @instrument, status: :created, location: @instrument
-            else
-                render json: @instrument.errors, status: :unprocessable_entity
-            end
+      # PATCH/PUT /instruments/1
+      def update
+        unless current_user.role == 'admin'
+          return render json: { error: 'You do not have authorization' },
+                        status: :unauthorized
         end
-
-        # PATCH/PUT /instruments/1
-        def update
-            return render json: { error: 'You do not have authorization' }, status: :unauthorized unless current_user.role == 'admin'
-            if @instrument.update(instrument_params)
-                render json: @instrument
-            else
-                render json: @instrument.errors, status: :unprocessable_entity
-            end
+        if @instrument.update(instrument_params)
+          render json: @instrument
+        else
+          render json: @instrument.errors, status: :unprocessable_entity
         end
+      end
 
-        # DELETE /instruments/1
-        def destroy
-            return render json: { error: 'You do not have authorization' }, status: :unauthorized unless current_user.role == 'admin'
-            @instrument.destroy
+      # DELETE /instruments/1
+      def destroy
+        unless current_user.role == 'admin'
+          return render json: { error: 'You do not have authorization' },
+                        status: :unauthorized
         end
+        @instrument.destroy
+      end
 
-        private
+      private
 
-        # Use callbacks to share common setup or constraints between actions.
-        def set_instrument
-            @instrument = Instrument.find(params[:id])
-        rescue ActiveRecord::RecordNotFound
-            render json: { errors: 'Instrument not found' }, status: :not_found
-        end
+      # Use callbacks to share common setup or constraints between actions.
+      def set_instrument
+        @instrument = Instrument.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { errors: 'Instrument not found' }, status: :not_found
+      end
 
-        # Only allow a trusted parameter "white list" through.
-        def instrument_params
-            params.require(:instrument).permit(
-                :name, :description, :image, :price, :quantity, :category
-            )
-        end
+      # Only allow a trusted parameter "white list" through.
+      def instrument_params
+        params.require(:instrument).permit(
+          :name, :description, :image, :price, :quantity, :category
+        )
+      end
     end
   end
 end
--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -3,10 +3,10 @@ class Instrument < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :users, through: :reservations, dependent: :destroy
 
-  validates :name, presence: true, lengths => { maximum: 50 }
-  validates :description, presence: true, lengths => { maximum: 500 }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 500 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :category, presence: true, lengths => { maximum: 50 }
-  validates :image, presence: true, lengths => { maximum: 500 }, uniqueness: true
+  validates :category, presence: true, length: { maximum: 50 }
+  validates :image, presence: true, length: { maximum: 500 }, uniqueness: true
 end
