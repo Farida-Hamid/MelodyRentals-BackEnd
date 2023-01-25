abb57bd74e655e4b274a99c3d9df727cc044d1fd
Elfin Git <musongoletracy@gmail.com>
2023-01-25 16:59:20 +0200

    update user table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,5 @@
 # to create reservations, run the following in the rails console:
 # Reservation.create!(pickup_date: '2020-01-01', return_date: '2020-01-02', user_id: 1, instrument_id: 1)
 
-ser.create!(name: 'Admin User', username: 'admin', email: 'admin@mail.com', password: 'password', password_confirmation: 'password', role: 'admin')
+User.create!(name: 'Admin User', username: 'admin', email: 'admin@localhost', password: 'password', password_confirmation: 'password', role: 'admin')
+User.create!(name: 'Regular User', username: 'regular', email: 'regular@localhost', password: 'password', password_confirmation: 'password', role: 'user')
