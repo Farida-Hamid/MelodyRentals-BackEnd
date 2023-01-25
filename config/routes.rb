14ae280338419da07cb367e6f0815321a44c36b1
bantealem <bantem2021@gmail.com>
2023-01-25 17:01:28 +0300

    "Update Reservation Cotroller"

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  post '/auth/login', to: 'authentication#login'
+  get '/auth/login', to: 'authentication#login'
 
   namespace :api do
     namespace :v1 do
       resources :users
       resources :instruments
+      resources :reservations, only: [:index, :create, :destroy]
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,7 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   Character.create(name: "Luke", movie: movi
+# user = User.create!(name: "selamawit", username: "selam", email: " sela@example.com", password: "123456", role: "admin")
+user  = User.create!(name: "Meshu", username: "Meshu", email: "meshu@gmail.com", password: "123456", role: "admin")
+instrument = Instrument.create!(name: "guitar", description: "guitargxdytgxfrydh", image: "guitaruetsgdrfhcvgjbzwryxetc", category: "guitarszgxdctfv", price: 100, quantity: 10, user_id: user.id)
