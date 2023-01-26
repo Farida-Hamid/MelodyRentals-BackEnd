ac71679042aec7a19fe8fe9674b278b8d9de89bb
bantealem <bantem2021@gmail.com>
2023-01-26 11:28:38 +0300

    Update Validation

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -6,7 +6,7 @@ class Instrument < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 500 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :category, presence: true, length: { maximum: 50 }
   validates :image, presence: true, length: { maximum: 500 }
+  validates :available, inclusion: { in: [true, false] }
 end
