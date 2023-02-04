4c874f00557b57002efe00c552d5183442bec25c
Tracy Musongole <90801216+TracyMuso@users.noreply.github.com>
2023-02-04 07:59:43 +0200

    Update description length

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -4,7 +4,7 @@ class Instrument < ApplicationRecord
   has_many :users, through: :reservations, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :description, presence: true, length: { maximum: 500 }
+  validates :description, presence: true, length: { maximum: 1000 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :category, presence: true, length: { maximum: 50 }
   validates :image, presence: true, length: { maximum: 500 }
